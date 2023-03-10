d9a7252441e88253df5e5dc76a501bb87e87281f
el-pirata-bueno <d.zharinov@bk.ru>
2023-03-10 22:45:16 +0300

    ДЗ спринт 13 (v.1)

--- app/src/main/java/com/practicum/playlistmaker/Track.kt
+++ app/src/main/java/com/practicum/playlistmaker/Track.kt
@@ -1,14 +1,25 @@
 package com.practicum.playlistmaker
 
-data class Track (val trackName: String,
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Track  (val trackName: String,
                   val artistName: String,
                   val trackId: Int,
                   val trackTimeMillis: Int,
-                  val artworkUrl100: String)
+                  val artworkUrl100: String,
+                  val collectionName: String,
+                  val releaseDate: String,
+                  val primaryGenreName: String,
+                  val country: String) : Parcelable
 
      // trackName - название композиции
      // artistName - имя исполнителя
      // trackId - уникальный id трека
      // trackTime - продолжительность трека
      // artworkUrl100 - ссылка на изображение обложки
-
+     // collectionName - название альбома
+     // releaseDate - год релиза трека
+     // primaryGenreName - жанр трека
+     // country - страна исполнителя
